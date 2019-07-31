473b0af77cbff40ad33a36068e770b449fde9378
qiushi.xu <qiushi.xu@17zuoye.com>
2019-07-31 19:02:22 +0800

    简单模型训练写好

--- simple_predict.py
+++ simple_predict.py
@@ -10,13 +10,12 @@ class SimplePredict(object):
 
     @classmethod
     def init_model(cls):
-        return RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
+        return RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0, verbose=1)
 
     def train_model(self):
         x = self.data.iloc[:, 2:].values
         y = self.data.iloc[:, 1].astype(int).values
         self.model.fit(x, y)
-        print("x")
 
 
 if __name__ == "__main__":
