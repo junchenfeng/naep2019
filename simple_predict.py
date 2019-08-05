643deba9742221912da8f612b561e70353182329
qiushi.xu <qiushi.xu@17zuoye.com>
2019-08-05 18:48:06 +0800

    增加参数

--- simple_predict.py
+++ simple_predict.py
@@ -1,17 +1,25 @@
 import os
 from datetime import date
+from sklearn.metrics import cohen_kappa_score
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_predict, cross_val_score
 import pandas as pd
+import numpy as np
 from data_prepare.data_processor import SimpleTestProcessor
 
 RESULT_DIR = "data/result"
 HIDDEN_30_NAME = "hidden_30_result.csv"
 TOTAL_HIDDEN_NAME = f"total_hidden_{date.today()}.csv"
+RANDOM_STATE = 1
+DEFAULT_SCORE_METHOD = "roc_auc"
+DEFAULT_CV = 3
+DEFAULT_VERBOSE = 1
+DEFAULT_N_JOBS = 1
 
 
 class SimplePredict(object):
     def __init__(self):
-        self.train_data = SimpleTestProcessor().get_train_time_period_df()
+        self.train_data_and_train_label = SimpleTestProcessor().get_train_time_period_df()
         self.hidden_data = SimpleTestProcessor().get_total_hidden_period_df()
         self.hidden_label = SimpleTestProcessor().hidden_label
         self.model = self.init_model()
@@ -21,9 +29,11 @@ class SimplePredict(object):
         return RandomForestClassifier(n_estimators=1000, random_state=0, verbose=1)
 
     def train_predict_model(self):
-        train_data, hidden_data = self.balance_train_and_hidden(self.train_data.iloc[:, 2:], self.hidden_data)
+        train_data, hidden_data = self.balance_train_and_hidden(
+            self.train_data_and_train_label.iloc[:, 2:], self.hidden_data
+        )
         x = train_data.values
-        y = self.train_data.iloc[:, 1].astype(int).values
+        y = self.train_data_and_train_label.iloc[:, 1].astype(int).values
         self.model.fit(x, y)
         result = self.model.predict_proba(hidden_data.values)
         result_df = pd.DataFrame(
@@ -36,6 +46,26 @@ class SimplePredict(object):
             header=False,
             line_terminator=",\n",
         )
+
+    def get_model_auc(self) -> float:
+        train_data, hidden_data = self.balance_train_and_hidden(
+            self.train_data_and_train_label.iloc[:, 2:], self.hidden_data
+        )
+        x = train_data.values
+        y = self.train_data_and_train_label.iloc[:, 1].astype(int).values
+        cv_results = cross_val_score(
+            self.model, x, y, scoring=DEFAULT_SCORE_METHOD, cv=DEFAULT_CV
+        )
+        return 2 * float(np.mean(cv_results)) - 0.5
+
+    def get_model_kappa(self) -> float:
+        train_data, hidden_data = self.balance_train_and_hidden(
+            self.train_data_and_train_label.iloc[:, 2:], self.hidden_data
+        )
+        x = train_data.values
+        y = self.train_data_and_train_label.iloc[:, 1].astype(int).values
+        y_predict = cross_val_predict(self.model, x, y, cv=DEFAULT_CV)
+        return cohen_kappa_score(y, y_predict)
 
     @classmethod
     def balance_train_and_hidden(cls, train, hidden):
