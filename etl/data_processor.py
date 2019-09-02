cfe708e2fba6c417ba3bf0cd47df1fbb7ff6e50c
qiushi.xu <qiushi.xu@17zuoye.com>
2019-09-02 13:41:14 +0800

    修改参数

--- etl/data_processor.py
+++ etl/data_processor.py
@@ -376,6 +376,7 @@ class FeatureProcessor(object):
             .pipe(cls.add_verb_attempt_duration)
             .pipe(cls.add_duration_level, None, VERB_DURATION)
         )
+        middle_df = middle_df.query("duration_level<10")
         verb_attempt_chain = middle_df.groupby(
             [INDEX_VAR, "occur_minute"], sort=False
         ).agg({"Observable": list, "ItemType": list, "duration_level": "sum"})
